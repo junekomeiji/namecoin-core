6b0820abb17ea57bf070a9e7e3e8d0ef14729982
June Egbert <102435854+junekomeiji@users.noreply.github.com>
2025-03-03 23:07:50 +0100

    responded to PR, still not working perfectly though

--- src/names/applications.h
+++ src/names/applications.h
@@ -29,7 +29,7 @@ bool IsMinimalJSONOrEmptyString (const std::string& text);
 
 std::string GetMinimalJSON (const std::string& text);
 
-bool IsEndingWithBit (const std::string& domain);
+bool IsPurportedNamecoinDomain (const std::string& domain);
 
 std::string ASCIIFromDomain (const std::string& domain);
 
